013936b7fd4f204a3629bd6e657c6e274775a6dc
Kamil Dike <kamil.dike.96@gmail.com>
2020-10-19 21:18:02 +0200

    spring app initialization

---
+++ src/main/java/com/inzynieriaoprogramowania/inzynieriaoprogramowania/InzynieriaOprogramowaniaApplication.java
@@ -0,0 +1,13 @@
+package com.inzynieriaoprogramowania.inzynieriaoprogramowania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InzynieriaOprogramowaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InzynieriaOprogramowaniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inzynieriaoprogramowania/inzynieriaoprogramowania/InzynieriaOprogramowaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inzynieriaoprogramowania.inzynieriaoprogramowania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InzynieriaOprogramowaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
